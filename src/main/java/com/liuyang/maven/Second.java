9af79f23bb70265a8ef2b2abd68840126a33a55f
liuyang8091216 <liuyang8091216@126.com>
2017-02-05 08:14:50 +0800

    过年之后第一次提交

--- src/main/java/com/liuyang/maven/Second.java
+++ src/main/java/com/liuyang/maven/Second.java
@@ -1,5 +1,7 @@
 package com.liuyang.maven;
 
+import org.eclipse.jdt.internal.compiler.batch.Main;
+
 /**
  * master分支
  * @author 黄苛苛
@@ -9,5 +11,7 @@ package com.liuyang.maven;
 
 //继续修改
 public class Second {
-
+	public static void main(String[] args) {
+		System.out.println("过年来学第一次提交码云~");
+	}
 }
