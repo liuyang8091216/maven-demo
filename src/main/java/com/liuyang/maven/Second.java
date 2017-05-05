40c79750dd158419352905326b752b0713c8a378
liuyang8091216 <liuyang8091216@126.com>
2017-05-05 20:41:02 +0800

    sc

--- src/main/java/com/liuyang/maven/Second.java
+++ src/main/java/com/liuyang/maven/Second.java
@@ -13,5 +13,37 @@ import org.eclipse.jdt.internal.compiler.batch.Main;
 public class Second {
 	public static void main(String[] args) {
 		System.out.println("过年来学第一次提交码云~");
-	}
+	    Integer a = 127;  
+	    Integer b = 127;  
+	    System.out.println("等于127:");  
+	    System.out.println(a == b);  
+	    System.out.println("*****************");  
+	  
+	    a = 128;  
+	    b = 128;  
+	    System.out.println("等于128:");  
+	    System.out.println(a == b);  
+	    System.out.println("*****************");  
+	  
+	    a = -128;  
+	    b = -128;  
+	    System.out.println("等于-128:");  
+	    System.out.println(a == b);  
+	    System.out.println("*****************");  
+	  
+	    a = -129;  
+	    b = -129;  
+	    System.out.println("等于-129:");  
+	    System.out.println(a == b);  
+	    System.out.println("*****************");  
+	  
+	    // 测试Boolean  
+	    System.out.println("测试Boolean");  
+	    Boolean c = true;  
+	    Boolean d = true;  
+	    System.out.println(c == d);  
+	    d = new Boolean(true);  
+	    System.out.println(c == d);  
+		}  
+	
 }
